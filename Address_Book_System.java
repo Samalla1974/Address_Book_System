349078462cfaa78c0060a041625f4583525c832b
Samalla1974 <94819969+Samalla1974@users.noreply.github.com>
2021-12-19 21:43:29 +0530

    Add files via upload

--- Address_Book_System.java
+++ Address_Book_System.java
@@ -1,10 +1,9 @@
 package usecases;
 
 import java.util.Scanner;
-public class Address_Book_System {
-	
 
- public class Contacts extends Address_Book_System {
+ public class Address_Book_System
+ {
 public static void main(String[] args) {
 	 
 Scanner sc = new Scanner (System.in);  
@@ -39,7 +38,43 @@ void addAPerson() {
 	String zip =sc.next();
 	
 	System.out.println("Enter phone number: ");
-	String phoneNumber = sc.next();
+	String phoneNumber = sc.nextLine();
+}
+	
+	void edit() {
+		contacts e=new contacts();
+		System.out.println("What do you want to edit?");
+		System.out.println("1. Address");
+		System.out.println("2. Phone Number");
+		
+		Scanner sc = new Scanner (System.in);  
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+			System.out.print("Enter address: ");
+			sc.nextLine();
+			
+			String address = sc.nextLine();
+			System.out.print("Enter city, state and zip: ");
+			String city = sc.nextLine();
+			String state = sc.next();
+			String zip = sc.next();
+			break;
+
+		case 2:
+			System.out.println("Enter phone number: ");
+			String phoneNumber = sc.next();
+			e.put("phone",phoneNumber);
+			break;
+			
+			
+			e.put("Address", address);
+			e.put("City", city);
+			e.put("State", state);
+			e.put("Zip", zip);
+			break;
+
+		}
 }
 }
 }
