da39f79026f8aba73edaa0085f8514ff28dd2234
Samalla1974 <94819969+Samalla1974@users.noreply.github.com>
2021-12-19 19:54:46 +0530

    Add files via upload

---
+++ Address_Book_System.java
@@ -0,0 +1,27 @@
+package usecases;
+
+import java.util.Scanner;
+public class Address_Book_System {
+	
+
+ public class Contacts extends Address_Book_System {
+public static void main(String[] args) {
+	 
+Scanner sc = new Scanner (System.in);  
+int zip= sc.nextInt();  
+int number= sc.nextInt();
+String first_name= sc.nextLine();
+System.out.println("first_name");
+String last_name= sc.nextLine();
+System.out.println("last_name");
+String address= sc.nextLine();
+System.out.println("address");
+String city = sc.nextLine();
+System.out.println("city");
+String state= sc.nextLine();
+System.out.println("state");
+String email= sc.nextLine();    
+System.out.println("email");
+	}
+}
+}
